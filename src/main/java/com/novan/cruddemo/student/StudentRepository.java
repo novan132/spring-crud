d9d2be55caf75327977845ebbc10ce6228cb1156
Novan <l4novan132@gmail.com>
2023-07-30 16:10:23 +0700

    add db student repository

---
+++ src/main/java/com/novan/cruddemo/student/StudentRepository.java
@@ -0,0 +1,8 @@
+package com.novan.cruddemo.student;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StudentRepository extends JpaRepository<Student, Integer> {
+    Student findByEmail(String email);
+    void deleteByEmail(String email);
+}
