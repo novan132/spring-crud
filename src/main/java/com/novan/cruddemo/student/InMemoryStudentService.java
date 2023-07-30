6b0932b6b8794508fcbdf452bb5ac701867d7bcd
Novan <l4novan132@gmail.com>
2023-07-30 14:37:26 +0700

    feat: create interface and InMemory implementation for student service

---
+++ src/main/java/com/novan/cruddemo/student/InMemoryStudentService.java
@@ -0,0 +1,29 @@
+package com.novan.cruddemo.student;
+
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Service
+public class InMemoryStudentService implements StudentService{
+    @Override
+    public List<Student> findAllStudents() {
+        return List.of(
+                new Student(
+                        "Novan",
+                        "Ariadi",
+                        LocalDate.now(),
+                        "novan@mail.com",
+                        40
+                ),
+                new Student(
+                        "Ali",
+                        "Nafiah",
+                        LocalDate.now(),
+                        "ali@mail.com",
+                        42
+                )
+        );
+    }
+}
--- src/main/java/com/novan/cruddemo/student/StudentService.java
+++ src/main/java/com/novan/cruddemo/student/StudentService.java
@@ -5,24 +5,6 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDate;
 import java.util.List;
 
-@Service
-public class StudentService {
-    public List<Student> findAllStudents() {
-        return List.of(
-                new Student(
-                        "Novan",
-                        "Ariadi",
-                        LocalDate.now(),
-                        "novan@mail.com",
-                        40
-                ),
-                new Student(
-                        "Ali",
-                        "Nafiah",
-                        LocalDate.now(),
-                        "ali@mail.com",
-                        42
-                )
-        );
-    }
+public interface StudentService {
+    List<Student> findAllStudents();
 }
