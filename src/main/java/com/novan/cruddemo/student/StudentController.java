5c2e6af9730661d8b9dcf30ab0fea769421796c9
Novan <l4novan132@gmail.com>
2023-07-30 14:30:26 +0700

    refactor studentservice used with constructor injection

--- src/main/java/com/novan/cruddemo/student/StudentController.java
+++ src/main/java/com/novan/cruddemo/student/StudentController.java
@@ -3,6 +3,7 @@ package com.novan.cruddemo.student;
 import java.time.LocalDate;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,7 +11,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/students")
 public class StudentController {
-    private StudentService service = new StudentService();
+//    @Autowired
+    private StudentService service;
+
+    public StudentController(StudentService service) {
+        this.service = service;
+    }
+
     @GetMapping
     public List<Student> findAllStudents() {
         return service.findAllStudents();
