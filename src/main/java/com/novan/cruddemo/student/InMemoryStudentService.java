37dfb12654a00f2cbd5535d1c0d2cb30ac13636c
Novan <l4novan132@gmail.com>
2023-07-30 14:51:06 +0700

    feat: use in memory student repository in service

--- src/main/java/com/novan/cruddemo/student/InMemoryStudentService.java
+++ src/main/java/com/novan/cruddemo/student/InMemoryStudentService.java
@@ -2,33 +2,38 @@ package com.novan.cruddemo.student;
 
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDate;
 import java.util.List;
 
 @Service
 public class InMemoryStudentService implements StudentService{
+    private final InMemoryStudentDao dao;
+
+    public InMemoryStudentService(InMemoryStudentDao dao) {
+        this.dao = dao;
+    }
+
     @Override
     public Student save(Student s) {
-        return null;
+        return this.dao.save(s);
     }
 
     @Override
     public List<Student> findAllStudents() {
-        return null;
+        return dao.findAllStudents();
     }
 
     @Override
     public Student findByEmail(String email) {
-        return null;
+        return dao.findByEmail(email);
     }
 
     @Override
     public Student update(Student s) {
-        return null;
+        return dao.update(s);
     }
 
     @Override
     public void delete(String email) {
-
+        this.dao.delete(email);
     }
 }
